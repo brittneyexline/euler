b413c4383835d7e963139072c83d35f9511f47a8
Brittney Exline <bexline@nanigans.com>
2015-12-13 00:50:15 -0500

    final state for problem 26

--- problem26.py
+++ problem26.py
@@ -1,6 +1,7 @@
 #!/usr/local/bin/python
 #coding: utf8
 import math
+import re
 from decimal import *
 
 # A unit fraction contains 1 in the numerator.
@@ -19,6 +20,30 @@ from decimal import *
 # Find the value of d < 1000 for which 1/d contains the
 # longest recurring cycle in its decimal fraction part.
 
-getcontext().prec = 150
+getcontext().prec = 5000
+longest_substring_ever = ''
+x_with_longest_substring = 0
 for x in range(2, 1000):
-	print str(x) + ': ' + str(Decimal(1) / Decimal(x))
+	print 'Calculating substring for x = ' + str(x)
+	decimal_string = str(Decimal(1) / Decimal(x))
+	decimal_string = decimal_string[2:]
+	print decimal_string
+	longest_substring = '';
+	current_substring = '';
+	for i in range(3, (len(decimal_string)-1)/3):
+		for j in range(i, (len(decimal_string)-1)/3):
+			current_substring = decimal_string[i:j+1]
+			# print str(i) + ', ' + str(j) + ': ' + current_substring
+			if (re.search(current_substring, decimal_string[j+1:]) and len(current_substring) > len(longest_substring)):
+				if (current_substring == decimal_string[j+1:j+1+j+1-i] and current_substring == decimal_string[j+1+j+1-i:j+1+j+1-i+j+1-i]):
+					longest_substring = current_substring
+					break
+			else:
+				break
+	if (longest_substring == ''):
+		print '!!!!!!!!OMG!!!!!!!! no longest substring found for ' + str(x)
+	print 'longest substring: ' + str(longest_substring)
+	if (len(longest_substring) > len(longest_substring_ever)):
+		longest_substring_ever = longest_substring
+		x_with_longest_substring = x
+print 'x = ' + str(x_with_longest_substring) + ' has longest substring: ' + longest_substring_ever
